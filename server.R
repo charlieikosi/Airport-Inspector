e15ca0c11e0318fde73e7236b4a93551d018c530
charlieikosi <73019564+charlieikosi@users.noreply.github.com>
2022-06-21 23:07:52 +1000

    Update server.R

--- server.R
+++ server.R
@@ -107,7 +107,7 @@ server <-function(input, output, session) {
     predictionVar <- tibble(Latitude = as.numeric(input$latitude),
                             Longitude = as.numeric(input$longitude),
                             Elevation = as.numeric(input$elevation))
-    result <- predict(fit.rf, predictionVar) %>% as.character()
+    result <- predict(load("fit.rf.rda"), predictionVar) %>% as.character()
   })
   
   output$prediction_result <- renderText({
