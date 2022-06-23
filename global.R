0771a20bb21c25ac7c59214e29dce1c7e4728682
charlieikosi <73019564+charlieikosi@users.noreply.github.com>
2022-06-23 16:39:07 +1000

    Update global.R

--- global.R
+++ global.R
@@ -29,12 +29,7 @@ source("Filter_Density_Function.R")
 pngmap <- st_read("arcgis/png_adm_nso_20190508_shp/png_admbnda_adm1_nso_20190508.shp")
 var <- tm_shape(pngmap)+ tm_fill("IQR",id="ADM1_EN", alpha = 0.5) + tm_view(set.view = c(145.920000,-6.500000,5.5)) + tm_polygons(alpha = 0.1)
 
-# Formatted Table for Display in DB
-db_table <- airstrip
-    colnames(db_table) <- c("ICAO", "Airstrip Name", "Airstrip Type", "Latitude",
-                            "Longitude", "Elevation (ft)", "Municipality", "LLG PCode",
-                            "LLG", "District PCode", "District", "Province PCode",
-                            "Province")
+
 
 # Below is the required data for the map:
 airstrip <- read_csv("data/airports_geocoded.csv")# this is the data from which the airstrip information is obtained
@@ -43,6 +38,13 @@ airstrip_sf <- st_as_sf(airstrip, coords = c("longitude_", "latitude_d"), crs = 
 # data required for static info box.
 airstrip_count <- count(airstrip, "name") # counted the number of airstrips in the column titled "name".
 total_count <- airstrip_count %>% dplyr::select(n) %>% filter(n == airstrip_count$n) # Then filtered to only display the value of the count
+
+# Formatted Table for Display in DB
+db_table <- airstrip
+    colnames(db_table) <- c("ICAO", "Airstrip Name", "Airstrip Type", "Latitude",
+                            "Longitude", "Elevation (ft)", "Municipality", "LLG PCode",
+                            "LLG", "District PCode", "District", "Province PCode",
+                            "Province")
 
 # data required for dynamic info box
 survey_strip <- read_csv("data/airports_survey_report.csv") %>% st_as_sf(coords = c("longitude_", "latitude_d"), crs = 4326)
